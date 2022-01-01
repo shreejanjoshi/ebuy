d43509016be13c9206618ea3e1c81825c9c57c81
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-01 20:44:11 +0100

    fixed user database problem

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,170 +3,113 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User {
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $first_name;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $last_name;
-
-    #[ORM\Column(type: 'string', length: 255, unique: true)]
-    private $username;
-
-    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $email;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
     private $password;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $address_line;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $city;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $post_code;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $country;
-
-    #[ORM\OneToMany(mappedBy: 'seller_id', targetEntity: Product::class, orphanRemoval: true)]
-    private $products;
-
-    public function __construct() {
-        $this->products = new ArrayCollection();
-    }
-
-    public function getId(): ?int {
+    public function getId(): ?int
+    {
         return $this->id;
     }
 
-    public function getFirstName(): ?string {
-        return $this->first_name;
-    }
-
-    public function setFirstName(string $first_name): self {
-        $this->first_name = $first_name;
-
-        return $this;
-    }
-
-    public function getLastName(): ?string {
-        return $this->last_name;
-    }
-
-    public function setLastName(string $last_name): self {
-        $this->last_name = $last_name;
-
-        return $this;
-    }
-
-    public function getUsername(): ?string {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string {
+    public function getEmail(): ?string
+    {
         return $this->email;
     }
 
-    public function setEmail(string $email): self {
+    public function setEmail(string $email): self
+    {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getPassword(): ?string {
-        return $this->password;
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
     }
 
-    public function setPassword(string $password): self {
-        $this->password = $password;
-
-        return $this;
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
     }
 
-    public function getAddressLine(): ?string {
-        return $this->address_line;
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
-    public function setAddressLine(string $address_line): self {
-        $this->address_line = $address_line;
-
-        return $this;
-    }
-
-    public function getCity(): ?string {
-        return $this->city;
-    }
-
-    public function setCity(string $city): self {
-        $this->city = $city;
-
-        return $this;
-    }
-
-    public function getPostCode(): ?string {
-        return $this->post_code;
-    }
-
-    public function setPostCode(string $post_code): self {
-        $this->post_code = $post_code;
-
-        return $this;
-    }
-
-    public function getCountry(): ?string {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): self {
-        $this->country = $country;
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
     /**
-     * @return Collection|Product[]
+     * @see PasswordAuthenticatedUserInterface
      */
-    public function getProducts(): Collection {
-        return $this->products;
+    public function getPassword(): string
+    {
+        return $this->password;
     }
 
-    public function addProduct(Product $product): self {
-        if (!$this->products->contains($product)) {
-            $this->products[] = $product;
-            $product->setSeller($this);
-        }
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
         return $this;
     }
 
-    public function removeProduct(Product $product): self {
-        if ($this->products->removeElement($product)) {
-            // set the owning side to null (unless already changed)
-            if ($product->getSeller() === $this) {
-                $product->setSeller(null);
-            }
-        }
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
 
-        return $this;
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
