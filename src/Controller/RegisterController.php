2095cbbec4a6d85e0bb4abb67b435929b667854c
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-03 07:34:57 +0100

    made register file but its error

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'register')]
-    public function index(): Response
+    #[Route('/reg', name: 'register')]
+    public function reg(): Response
     {
+        $regform = $this->createFormBuilder()
+        
+        ->add('first_name', TextType::class,[
+            'label' => 'First name'])
+        ->add('last_name', TextType::class,[
+            'label' => 'Last name'])
+        ->add('email', TextType::class,[
+            'label' => 'Email'])
+        ->add('username', TextType::class,[
+            'label' => 'Username'])
+        ->add('country', TextType::class,[
+            'label' => 'Country'])
+        ->add('city', TextType::class,[
+            'label' => 'City'])
+        ->add('post_code', TextType::class,[
+            'label' => 'Post code'])
+        ->add('address_line', TextType::class,[
+            'label' => 'Street name and number'])
+        ->add('password', RepeatedType::class,[
+            'type' => RepeatedType::class,
+            'required' => true,
+            'first_options' => ['label' => 'Password'],
+            'second_options' => ['label' => 'Repeat Password']
+        ])
+
+        ->add('register', SubmitType::class)
+        ->getForm()
+        ;
+
+
+
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'regform' => $regform->createView()
         ]);
     }
 }
