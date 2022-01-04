605fc106ff3501060f500941ab41767a6b029a7c
Christiaan van der Woude <ch_vdw@outlook.be>
2022-01-04 13:22:48 +0100

    changed form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
 
-    public function contact(): Response
+    public function contact()
     {
-        return $this->render('contact/index.html.twig');
+        $form = $this->createForm(ContactType::class);
+        return $this->render('contact/contact.html.twig', [
+            'our_form' => $form->createView()
+        ]);
     }
+
 }
