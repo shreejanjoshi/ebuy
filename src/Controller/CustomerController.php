42d1f9394e7898680dfbf9ed30f218aab5d4eb3e
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-01-04 12:26:31 +0100

    testing the dadabase, not required to merge with main

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -6,23 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class CustomerController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/customer', name: 'customer')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('customer/index.html.twig', [
+            'controller_name' => 'CustomerController',
             'name' => 'Lakneswaran',
             'hobbies' => [
                 'gaming',
-                'programming'
-            ],
-            'age' => 30,
-            // 'customer' => $customer,
-            // 'name' => $customer->getName()
+                'Programming'
+            ]
         ]);
-
-        
     }
 }
