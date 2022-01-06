8ce1b6f349c2112c1ecaadd508db8424114e8a82
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-06 11:52:59 +0100

    now if user register then mail will be send

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -12,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,41 +21,48 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegisterController extends AbstractController
 {
     #[Route('/reg', name: 'register')]
-    public function reg(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passHasher): Response
+    public function reg(MailerInterface $mailer, ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passHasher): Response
     {
         $regform = $this->createFormBuilder()
-        
-        ->add('first_name', TextType::class,[
-            'label' => 'First name'])
-        ->add('last_name', TextType::class,[
-            'label' => 'Last name'])
-        ->add('email', TextType::class,[
-            'label' => 'Email'])
-        ->add('username', TextType::class,[
-            'label' => 'Username'])
-        ->add('country', TextType::class,[
-            'label' => 'Country'])
-        ->add('city', TextType::class,[
-            'label' => 'City'])
-        ->add('post_code', TextType::class,[
-            'label' => 'Post code'])
-        ->add('address_line', TextType::class,[
-            'label' => 'Street name and number'])
-        ->add('password', RepeatedType::class,[
-            'type' => PasswordType::class,
-            'required' => true,
-            'first_options' => ['label' => 'Password'],
-            'second_options' => ['label' => 'Repeat Password']
-        ])
 
-        ->add('register', SubmitType::class)
-        ->getForm()
-        ;
+            ->add('first_name', TextType::class, [
+                'label' => 'First name'
+            ])
+            ->add('last_name', TextType::class, [
+                'label' => 'Last name'
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email'
+            ])
+            ->add('username', TextType::class, [
+                'label' => 'Username'
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Country'
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'City'
+            ])
+            ->add('post_code', TextType::class, [
+                'label' => 'Post code'
+            ])
+            ->add('address_line', TextType::class, [
+                'label' => 'Street name and number'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password']
+            ])
+
+            ->add('register', SubmitType::class)
+            ->getForm();
 
         // process request
         $regform->handleRequest($request);
 
-        if($regform->isSubmitted()){
+        if ($regform->isSubmitted()) {
             // get data
             $input = $regform->getData();
             // save data to database
@@ -69,7 +78,7 @@ class RegisterController extends AbstractController
             $user->setAddressLine($input['address_line']);
 
             $user->setPassword(
-                $passHasher ->hashPassword($user, $input['password'])
+                $passHasher->hashPassword($user, $input['password'])
             );
 
             //entity manager
@@ -78,6 +87,22 @@ class RegisterController extends AbstractController
             $em->persist($user);
             // sendit
             $em->flush();
+
+            $email = (new Email())
+                ->from('customerservices@ebuy.com')
+                ->to($user->getEmail())
+                ->subject('Welcome to the Ebuy!')
+                ->text("
+                Dear {$user->getFirstName()},
+                
+                Nice to meet you {$user->getFirstName()}! Feel free to buy and sell your products.
+                If you have any question or problem don't hesitate to reach out to us.
+                
+                best regards
+                customerservices@ebuy.com")
+                ->html("<h1>Dear {$user->getFirstName()}, <br><br>Nice to meet you {$user->getFirstName()}! Feel free to buy and sell your products. <br>If you have any question or problem don't hesitate to reach out to us. <br><br>best regards<br>customerservices@ebuy.com</h1>");
+
+            $mailer->send($email);
 
             return $this->redirect($this->generateUrl('home'));
         }
