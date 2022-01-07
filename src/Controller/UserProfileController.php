44b30f5b6aa03f40d73cc1476bd252f54c9e5ed7
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-07 08:34:46 +0100

    made a userProfileController to show user data

---
+++ src/Controller/UserProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserProfileController extends AbstractController
+{
+    #[Route('/my-profile', name: 'myProfile')]
+    public function index(): Response
+    {
+        return $this->render('user_profile/index.html.twig', [
+            'controller_name' => 'UserProfileController',
+        ]);
+    }
+}
